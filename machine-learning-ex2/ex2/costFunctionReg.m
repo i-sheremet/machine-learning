3ec4faa49374dc7052539a7ff9be940fffd6096f
Ilia <illia.sheremet@gmail.com>
2017-09-03 18:43:11 +0100

    costFunctionReg.m corrected for theta non-reg for Cost Function as well

--- machine-learning-ex2/ex2/costFunctionReg.m
+++ machine-learning-ex2/ex2/costFunctionReg.m
@@ -12,15 +12,16 @@ J = 0;
 grad = zeros(size(theta));
 
 % ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost of a particular choice of theta.
-%               You should set J to the cost.
-%               Compute the partial derivatives and set grad to the partial
-%               derivatives of the cost w.r.t. each parameter in theta
 
+% Multiply theta to this vector so that it's not regularize theta(0):
+% https://www.coursera.org/learn/machine-learning/resources/Zi29t - check after: "Note Well: The second sum," ... 
+non_reg_first_parameter = ones(size(theta));
+non_reg_first_parameter(1) = 0;
 
+h = sigmoid(X * theta);
+J = (1 / m) * (-y' * log(h) - (1 - y)' * log(1 - h)) + (lambda / (2 * m)) * sum(theta.^2 .* non_reg_first_parameter);
 
-
-
+grad = ((1 / m) * sum((h - y) .* X))' .+ (lambda / m) * (theta .* non_reg_first_parameter);
 
 % =============================================================
 
