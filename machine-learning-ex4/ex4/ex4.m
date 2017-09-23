a2109030ee0c6c8766da59dd93edb9d2defec3ed
Ilia <illia.sheremet@gmail.com>
2017-09-23 22:13:41 +0100

    finished

--- machine-learning-ex4/ex4/ex4.m
+++ machine-learning-ex4/ex4/ex4.m
@@ -178,35 +178,35 @@ pause;
 %  is a function which works similarly to "fminunc". Recall that these
 %  advanced optimizers are able to train our cost functions efficiently as
 %  long as we provide them with the gradient computations.
-%
-% fprintf('\nTraining Neural Network... \n')
 
-% %  After you have completed the assignment, change the MaxIter to a larger
-% %  value to see how more training helps.
-% options = optimset('MaxIter', 50);
+fprintf('\nTraining Neural Network... \n')
 
-% %  You should also try different values of lambda
-% lambda = 1;
+%  After you have completed the assignment, change the MaxIter to a larger
+%  value to see how more training helps.
+options = optimset('MaxIter', 400);
 
-% % Create "short hand" for the cost function to be minimized
-% costFunction = @(p) nnCostFunction(p, ...
-%                                    input_layer_size, ...
-%                                    hidden_layer_size, ...
-%                                    num_labels, X, y, lambda);
+%  You should also try different values of lambda
+lambda = 0.5;
 
-% % Now, costFunction is a function that takes in only one argument (the
-% % neural network parameters)
-% [nn_params, cost] = fmincg(costFunction, initial_nn_params, options);
+% Create "short hand" for the cost function to be minimized
+costFunction = @(p) nnCostFunction(p, ...
+                                   input_layer_size, ...
+                                   hidden_layer_size, ...
+                                   num_labels, X, y, lambda);
 
-% % Obtain Theta1 and Theta2 back from nn_params
-% Theta1 = reshape(nn_params(1:hidden_layer_size * (input_layer_size + 1)), ...
-%                  hidden_layer_size, (input_layer_size + 1));
+% Now, costFunction is a function that takes in only one argument (the
+% neural network parameters)
+[nn_params, cost] = fmincg(costFunction, initial_nn_params, options);
 
-% Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):end), ...
-%                  num_labels, (hidden_layer_size + 1));
+% Obtain Theta1 and Theta2 back from nn_params
+Theta1 = reshape(nn_params(1:hidden_layer_size * (input_layer_size + 1)), ...
+                 hidden_layer_size, (input_layer_size + 1));
 
-% fprintf('Program paused. Press enter to continue.\n');
-% pause;
+Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):end), ...
+                 num_labels, (hidden_layer_size + 1));
+
+fprintf('Program paused. Press enter to continue.\n');
+pause;
 
 
 %% ================= Part 9: Visualize Weights =================
@@ -214,18 +214,18 @@ pause;
 %  displaying the hidden units to see what features they are capturing in 
 %  the data.
 
-% fprintf('\nVisualizing Neural Network... \n')
+fprintf('\nVisualizing Neural Network... \n')
 
-% displayData(Theta1(:, 2:end));
+displayData(Theta1(:, 2:end));
 
-% fprintf('\nProgram paused. Press enter to continue.\n');
-% pause;
+fprintf('\nProgram paused. Press enter to continue.\n');
+pause;
 
-%% ================= Part 10: Implement Predict =================
-%  After training the neural network, we would like to use it to predict
-%  the labels. You will now implement the "predict" function to use the
-%  neural network to predict the labels of the training set. This lets
-%  you compute the training set accuracy.
+% ================= Part 10: Implement Predict =================
+ % After training the neural network, we would like to use it to predict
+ % the labels. You will now implement the "predict" function to use the
+ % neural network to predict the labels of the training set. This lets
+ % you compute the training set accuracy.
 
 pred = predict(Theta1, Theta2, X);
 
