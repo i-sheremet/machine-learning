6360f1443dda341d6595b67087cb6aebacfd667e
Ilia <illia.sheremet@gmail.com>
2017-09-23 17:44:13 +0100

    Sigmoid Gradient implemented

--- machine-learning-ex4/ex4/ex4.m
+++ machine-learning-ex4/ex4/ex4.m
@@ -39,7 +39,7 @@ m = size(X, 1);
 sel = randperm(size(X, 1));
 sel = sel(1:100);
 
-displayData(X(sel, :));
+% displayData(X(sel, :));
 
 fprintf('Program paused. Press enter to continue.\n');
 pause;
@@ -126,13 +126,13 @@ pause;
 %  implementing a function to initialize the weights of the neural network
 %  (randInitializeWeights.m)
 
-fprintf('\nInitializing Neural Network Parameters ...\n')
+% fprintf('\nInitializing Neural Network Parameters ...\n')
 
-initial_Theta1 = randInitializeWeights(input_layer_size, hidden_layer_size);
-initial_Theta2 = randInitializeWeights(hidden_layer_size, num_labels);
+% initial_Theta1 = randInitializeWeights(input_layer_size, hidden_layer_size);
+% initial_Theta2 = randInitializeWeights(hidden_layer_size, num_labels);
 
-% Unroll parameters
-initial_nn_params = [initial_Theta1(:) ; initial_Theta2(:)];
+% % Unroll parameters
+% initial_nn_params = [initial_Theta1(:) ; initial_Theta2(:)];
 
 
 %% =============== Part 7: Implement Backpropagation ===============
@@ -141,13 +141,13 @@ initial_nn_params = [initial_Theta1(:) ; initial_Theta2(:)];
 %  code you've written in nnCostFunction.m to return the partial
 %  derivatives of the parameters.
 %
-fprintf('\nChecking Backpropagation... \n');
+% fprintf('\nChecking Backpropagation... \n');
 
-%  Check gradients by running checkNNGradients
-checkNNGradients;
+% %  Check gradients by running checkNNGradients
+% checkNNGradients;
 
-fprintf('\nProgram paused. Press enter to continue.\n');
-pause;
+% fprintf('\nProgram paused. Press enter to continue.\n');
+% pause;
 
 
 %% =============== Part 8: Implement Regularization ===============
@@ -155,21 +155,21 @@ pause;
 %  continue to implement the regularization with the cost and gradient.
 %
 
-fprintf('\nChecking Backpropagation (w/ Regularization) ... \n')
+% fprintf('\nChecking Backpropagation (w/ Regularization) ... \n')
 
-%  Check gradients by running checkNNGradients
-lambda = 3;
-checkNNGradients(lambda);
+% %  Check gradients by running checkNNGradients
+% lambda = 3;
+% checkNNGradients(lambda);
 
-% Also output the costFunction debugging values
-debug_J  = nnCostFunction(nn_params, input_layer_size, ...
-                          hidden_layer_size, num_labels, X, y, lambda);
+% % Also output the costFunction debugging values
+% debug_J  = nnCostFunction(nn_params, input_layer_size, ...
+%                           hidden_layer_size, num_labels, X, y, lambda);
 
-fprintf(['\n\nCost at (fixed) debugging parameters (w/ lambda = %f): %f ' ...
-         '\n(for lambda = 3, this value should be about 0.576051)\n\n'], lambda, debug_J);
+% fprintf(['\n\nCost at (fixed) debugging parameters (w/ lambda = %f): %f ' ...
+%          '\n(for lambda = 3, this value should be about 0.576051)\n\n'], lambda, debug_J);
 
-fprintf('Program paused. Press enter to continue.\n');
-pause;
+% fprintf('Program paused. Press enter to continue.\n');
+% pause;
 
 
 %% =================== Part 8: Training NN ===================
@@ -179,34 +179,34 @@ pause;
 %  advanced optimizers are able to train our cost functions efficiently as
 %  long as we provide them with the gradient computations.
 %
-fprintf('\nTraining Neural Network... \n')
+% fprintf('\nTraining Neural Network... \n')
 
-%  After you have completed the assignment, change the MaxIter to a larger
-%  value to see how more training helps.
-options = optimset('MaxIter', 50);
+% %  After you have completed the assignment, change the MaxIter to a larger
+% %  value to see how more training helps.
+% options = optimset('MaxIter', 50);
 
-%  You should also try different values of lambda
-lambda = 1;
+% %  You should also try different values of lambda
+% lambda = 1;
 
-% Create "short hand" for the cost function to be minimized
-costFunction = @(p) nnCostFunction(p, ...
-                                   input_layer_size, ...
-                                   hidden_layer_size, ...
-                                   num_labels, X, y, lambda);
+% % Create "short hand" for the cost function to be minimized
+% costFunction = @(p) nnCostFunction(p, ...
+%                                    input_layer_size, ...
+%                                    hidden_layer_size, ...
+%                                    num_labels, X, y, lambda);
 
-% Now, costFunction is a function that takes in only one argument (the
-% neural network parameters)
-[nn_params, cost] = fmincg(costFunction, initial_nn_params, options);
+% % Now, costFunction is a function that takes in only one argument (the
+% % neural network parameters)
+% [nn_params, cost] = fmincg(costFunction, initial_nn_params, options);
 
-% Obtain Theta1 and Theta2 back from nn_params
-Theta1 = reshape(nn_params(1:hidden_layer_size * (input_layer_size + 1)), ...
-                 hidden_layer_size, (input_layer_size + 1));
+% % Obtain Theta1 and Theta2 back from nn_params
+% Theta1 = reshape(nn_params(1:hidden_layer_size * (input_layer_size + 1)), ...
+%                  hidden_layer_size, (input_layer_size + 1));
 
-Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):end), ...
-                 num_labels, (hidden_layer_size + 1));
+% Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):end), ...
+%                  num_labels, (hidden_layer_size + 1));
 
-fprintf('Program paused. Press enter to continue.\n');
-pause;
+% fprintf('Program paused. Press enter to continue.\n');
+% pause;
 
 
 %% ================= Part 9: Visualize Weights =================
@@ -214,12 +214,12 @@ pause;
 %  displaying the hidden units to see what features they are capturing in 
 %  the data.
 
-fprintf('\nVisualizing Neural Network... \n')
+% fprintf('\nVisualizing Neural Network... \n')
 
-displayData(Theta1(:, 2:end));
+% displayData(Theta1(:, 2:end));
 
-fprintf('\nProgram paused. Press enter to continue.\n');
-pause;
+% fprintf('\nProgram paused. Press enter to continue.\n');
+% pause;
 
 %% ================= Part 10: Implement Predict =================
 %  After training the neural network, we would like to use it to predict
